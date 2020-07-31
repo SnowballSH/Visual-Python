8ce233452ad7e35eff98cd766c2a282187e45c60
SnowballSH <derek070211@gmail.com>
2020-07-31 16:45:48 -0500

    Writer.py first added

---
+++ Writer.py
@@ -0,0 +1,19 @@
+# Main Writer for master branch
+
+import os
+
+
+class Writer:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+        # Create file if not exist
+        if not os.path.exists(file_path):
+            with open(file_path, "x"):
+                pass
+
+        self.append_file = self.f = open(file_path, "a")
+
+    def close(self):
+        self.append_file.close()
+
