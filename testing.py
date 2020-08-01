768769d1cb409903d6d29f684e0dd3bec07a9e97
SnowballSH <derek070211@gmail.com>
2020-08-01 07:45:41 -0500

    Corrected testing.py to fit PEP

--- testing.py
+++ testing.py
@@ -1,16 +1,15 @@
-  
 import Writer
 
 w = Writer.Writer("test.py")
 
 w.print_("Hello", "World!")
 
-#Testing my var assignment
-w.assignVar("stringVar", "Hello!!", 'str')
-w.assignVar("stringVar2", "5", 'str')
-w.assignVar("intVar", "5", 'int')
-w.assignVar("floatVar", "5", 'float')
-w.assignVar("boolVar", "True", 'bool')
+# Testing my var assignment
+
+w.assign_var("stringVar", "Hello!!", 'str')
+w.assign_var("stringVar2", "5", 'str')
+w.assign_var("intVar", "5", 'int')
+w.assign_var("floatVar", "5", 'float')
+w.assign_var("boolVar", "True", 'bool')
 
 w.close()
-
