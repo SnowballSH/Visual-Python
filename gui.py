f7fb18f2e76324de0d8bf1e0d2bc5034ac984b57
SnowballSH <derek070211@gmail.com>
2020-08-03 17:42:35 -0500

    edited GUI

--- gui.py
+++ gui.py
@@ -8,3 +8,25 @@ import compiler
 # use modules from compiler so it doesn't need to be imported again
 json = compiler.json
 os = compiler.os
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = '%i,%i' % (80, 80)
+
+# Variables
+
+FPS = 120
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    win = pygame.display.set_mode((800, 600))
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+main()
