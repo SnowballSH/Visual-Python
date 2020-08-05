c67a8f46668b7f3d18a870d5c9be5b9ec04506d6
SnowballSH <derek070211@gmail.com>
2020-08-05 14:37:09 -0500

    Edited to fit PEP

--- gui.py
+++ gui.py
@@ -3,8 +3,8 @@
 # Imports
 
 import pygame
-import json
-import os
+# import json
+# import os
 from pygame.locals import *
 
 pygame.font.init()
@@ -23,9 +23,8 @@ BLOCK_FONT = pygame.font.SysFont("lemon", 25)
 
 
 class Block:
-
-    def __init__(self, x, y, color, name, func = None):
-        self.color = color
+    def __init__(self, x, y, colour, name, func=None):
+        self.color = colour
         self.x = x
         self.y = y
         self.w = 200
@@ -42,7 +41,8 @@ class Block:
         self.text_input.draw(win)
         win.blit(self.text, (self.x + 4, self.y + 4))
 
-class functionOptions:
+
+class FunctionOptions:
     def __init__(self, x, y, colour, name):
         self.colour = colour
         self.x = x
@@ -50,13 +50,14 @@ class functionOptions:
         self.w = 120
         self.h = 25
 
-        self.text=BLOCK_FONT.render(name, True, BLACK)
+        self.text = BLOCK_FONT.render(name, True, BLACK)
         self.text_input = TextInput(self.x + self.w - 85, self.y + self.h - 45, 80, 40)
 
     def draw(self, win):
         pygame.draw.rect(win, self.colour, (self.x, self.y, self.w, self.h))
-        self.text_input.draw(win,False)
+        self.text_input.draw(win, False)
         win.blit(self.text, (self.x + 4, self.y + 4))
+
 
 class TextInput:
     def __init__(self, x, y, w, h):
@@ -65,8 +66,8 @@ class TextInput:
         self.w = w
         self.h = h
 
-    def draw(self, win, drawRect=True):
-        if drawRect:
+    def draw(self, win, draw_rect=True):
+        if draw_rect:
             pygame.draw.rect(win, WHITE, (self.x, self.y, self.w, self.h))
 
 
@@ -75,11 +76,11 @@ class Tree:
         self.blocks = []
         self.len = len(self.blocks)
 
-    def append(self, block: Block):
+    def append(self, block):
         self.blocks.append(block)
         self.len = len(self.blocks)
 
-    def remove(self, block: Block):
+    def remove(self, block):
         self.blocks.remove(block)
         self.len = len(self.blocks)
 
@@ -90,7 +91,7 @@ def main():
 
         # TEST
         pygame.draw.circle(win, RED, (width // 2, height // 2), 10)
-        #print(option)
+        # print(option)
         if option == 0:
             for b in tree.blocks:
                 b.draw(win)
@@ -114,13 +115,13 @@ def main():
 
     tree.append(Block(50, 100, GREEN, "output", "print"))
 
-    tree1=Tree()
+    tree1 = Tree()
     tree1.append(Block(50, 150, RED, "sum", "sum"))
 
     options = Tree()
 
-    options.append(functionOptions(40, 25, (100, 40, 255), "built-in-func"))
-    options.append(functionOptions(200, 25, (100, 40, 255), "operators"))
+    options.append(FunctionOptions(40, 25, (100, 40, 255), "built-in-func"))
+    options.append(FunctionOptions(200, 25, (100, 40, 255), "operators"))
 
     option = 0
     while run:
@@ -128,10 +129,10 @@ def main():
         draw()
 
         mx, my = pygame.mouse.get_pos()
-        if 50 < mx < 170 and 25 < my < 50 and pygame.mouse.get_pressed()[0]:
+        if 170 > mx > 50 > my > 25 and pygame.mouse.get_pressed()[0]:
             option = 0
 
-        if 200<mx<320 and 25<my<50 and pygame.mouse.get_pressed()[0]:
+        if 200 < mx < 320 and 25 < my < 50 and pygame.mouse.get_pressed()[0]:
             option = 1
 
         for event in pygame.event.get():
