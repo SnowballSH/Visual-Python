8db4218d43a85db4a66c6dc766238882ff74770b
SnowballSH <derek070211@gmail.com>
2020-08-26 16:17:22 -0500

    Fixed a small bug that right click and scrolling does not move the blocks

--- gui.py
+++ gui.py
@@ -289,6 +289,8 @@ def main():
 
             # clicking
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                if not pygame.mouse.get_pressed()[0]:
+                    continue
                 x, y = pygame.mouse.get_pos()
 
                 # clearing all blocks
